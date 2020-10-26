6ffe08dd45c00fd43a8f548543d8559d0948276d
nehudaev <nehudaev@gmail.com>
2020-10-27 01:05:29 +0900

    App

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {View} from 'react-native';
+import TabNavigator from 'react-native-tab-navigator';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    return (
+      <View >
+        <TabNavigator>
+            <TabNavigator.Item>
+                <Text>Hi</Text>
+            </TabNavigator.Item>
+        </TabNavigator>
+      </View>
+    );
+  }
