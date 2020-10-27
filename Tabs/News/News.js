78c110c816eb14ba8b7cd37433d1ae2fae5b7558
nehudaev <nehudaev@gmail.com>
2020-10-27 21:27:13 +0900

    commit

--- Tabs/News/News.js
+++ Tabs/News/News.js
@@ -1,1 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
 
+export default class NewPage extends React.Component {
+    render () {
+        <View>
+            <Text>Hi</Text>
+        </View>
+    }
+}
