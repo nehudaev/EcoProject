472a828a9663366c86253270eb1152ae618effc5
nehudaev <nehudaev@gmail.com>
2020-10-29 20:35:10 +0900

    sfsd

--- Tabs/Map/Map.js
+++ Tabs/Map/Map.js
@@ -3,10 +3,10 @@ import {View} from 'react-native';
 
 export const Map = props => {
     return(
-        
+        <View>
             <Text>
                 Hi
             </Text>
-       
+        </View>  
     )
 }
