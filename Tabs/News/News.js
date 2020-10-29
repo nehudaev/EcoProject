076d1783ce1fcc00b42f9d8d32430e40579d65d2
nehudaev <nehudaev@gmail.com>
2020-10-29 20:28:04 +0900

    news

--- Tabs/News/News.js
+++ Tabs/News/News.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import {View, Button} from 'react-native';
+import {View, Button, SafeAreaView, Text} from 'react-native';
 
 export const News = props => {
     return(
-        <View>
-            
-            <Button title="Добавить" />
-        </View>
+        <SafeAreaView style={styles.MainContainer}>
+      <ScrollView
+      >
+        <Text style={styles.textTitle}>Статьи:</Text>
+       </ScrollView>
+  </SafeAreaView>
     )
 }
+const styles = StyleSheet.create({
+    textTitle :{fontSize:22,fontFamily: 'Roboto-M',fontWeight:'700', padding:10}
+});
